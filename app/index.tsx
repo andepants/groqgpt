bb86200b614492d23c20af8009088820497fbc05
Andrew Heim <andrewsheim@gmail.com>
2024-05-27 17:39:26 -0700

    animted screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import { Text, View } from "react-native";
+import AnimatedIntro from "@/components/AnimatedIntro";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Test</Text>
+      <AnimatedIntro />
     </View>
   );
 }
