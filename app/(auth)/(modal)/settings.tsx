dbd53ca9aaaafe5f5fe0d64bd950aee75c4f232e
Andrew Heim <andrewsheim@gmail.com>
2024-06-26 16:41:27 -0700

    adding delete account for clerk in settings page

--- app/(auth)/(modal)/settings.tsx
+++ app/(auth)/(modal)/settings.tsx
@@ -4,17 +4,37 @@ import { Storage } from '@/utils/Storage';
 import { useAuth } from '@clerk/clerk-expo';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Button, Modal } from 'react-native';
 import { useMMKVString } from 'react-native-mmkv';
+
+
 const Page = () => {
   const [key, setKey] = useMMKVString('apikey', Storage);
   const [organization, setOrganization] = useMMKVString('org', Storage);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const [apiKey, setApiKey] = useState('');
   const [org, setOrg] = useState('');
   const router = useRouter();
 
-  const { signOut } = useAuth();
+  const { signOut, userId } = useAuth();
+
+  const deleteUser = async () => {
+    console.log('Deleting user...');
+    // fetch request to clerk to delete user
+    const response = await fetch(`https://api.clerk.com/v1/users/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer YOUR_CLERK_API_KEY_HERE`,
+      },
+    });
+    if (response.ok) {
+      signOut();
+      setModalVisible(false);
+    }
+    console.log('response code: ', response.status, response);
+  }
 
   const saveApiKey = async () => {
     setKey(apiKey);
@@ -28,13 +48,45 @@ const Page = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {marginTop: 10}]}>
       <TouchableOpacity
         style={[defaultStyles.btn, { backgroundColor: Colors.primary }]}
         onPress={() => signOut()}
       >
         <Text style={styles.buttonText}>Sign Out</Text>
       </TouchableOpacity>
+      <TouchableOpacity
+        style={[defaultStyles.btn, { backgroundColor: Colors.primary, marginTop: 20 }]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.buttonText}>Delete Account</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Are you sure you want to delete your account?</Text>
+            <TouchableOpacity
+              style={[defaultStyles.btn, { backgroundColor: Colors.primary }]}
+              onPress={deleteUser}
+            >
+              <Text style={styles.textStyle}>Yes, Delete</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[defaultStyles.btn, { backgroundColor: Colors.primary, marginTop: 10}]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       {/* {key && key !== '' && (
         <>
           <Text style={styles.label}>You are all set!</Text>
@@ -101,6 +153,39 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontSize: 16,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 export default Page;
